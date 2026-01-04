7e117279e20a60e9fc263077078cc303c3840915
Skywaterr <jleung12333@gmail.com>
2026-01-04 00:54:18 -0600

    Downloaded all necessary sprites and changed filters.js to output the Name field needed to match the sprite pngs

---
+++ draft-website/frontend/src/assets/renamer.py
@@ -0,0 +1,11 @@
+# I need to edit all the png names so they don't have "-" in them
+
+import os
+import glob
+
+files = glob.glob("*.png");
+
+for i in range(len(files)):
+    f = files[i]
+    os.rename(f, f.replace("-", ""))
+
